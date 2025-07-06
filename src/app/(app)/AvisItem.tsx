798cb787f57958a9a8286c57e411c0e6924db2f4
NeosiaNexus <devneosianexus@gmail.com>
2025-07-06 02:10:48 +0200

    chore: Lint

--- src/app/(app)/AvisItem.tsx
+++ src/app/(app)/AvisItem.tsx
@@ -12,7 +12,7 @@ export interface AvisItemProps {
 const AvisItem: React.FC<AvisItemProps> = ({ avis, auteur, role }): React.JSX.Element => {
   return (
     <div className="justify- flex w-[500px] flex-col items-center gap-4 text-center">
-      <p className='text-2xl'>&quot;{avis}&quot;</p>
+      <p className="text-2xl">&quot;{avis}&quot;</p>
       <div className="flex gap-2">
         <div className="h-6 w-6 rounded-full bg-white" /> - <span>{auteur}, </span>{' '}
         <span>{role}</span>
