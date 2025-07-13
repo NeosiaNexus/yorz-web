4b535dae8d186ffb98e290c08ed6b9492f922c40
NeosiaNexus <devneosianexus@gmail.com>
2025-07-13 22:12:59 +0200

    feat: Add sentry

--- src/actions/portfolio/item/upload-portfolio-item-action.ts
+++ src/actions/portfolio/item/upload-portfolio-item-action.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { createId } from '@paralleldrive/cuid2';
+import * as Sentry from '@sentry/nextjs';
 import { z } from 'zod';
 
 import { uploadFileAction } from '@/actions/cloud-storage-file';
@@ -25,68 +26,82 @@ const outputSchema = z.object({
 const uploadPortfolioItemAction = adminAction
   .inputSchema(inputSchema)
   .outputSchema(outputSchema)
-  .action(async ({ parsedInput: { categoryId, media } }) => {
-    if (categoryId) {
-      try {
-        const portFolioCategory = await prisma.portfolioCategory.findUnique({
-          where: {
-            id: categoryId,
-          },
-        });
-
-        if (!portFolioCategory) {
-          return {
-            success: false,
-            message: 'Catégorie non trouvée',
-          };
-        }
-      } catch {
-        return {
-          success: false,
-          message: 'Erreur lors de la récupération de la catégorie',
-        };
-      }
-    }
-
-    const cuid = createId();
-
-    const mediaUpload = await uploadFileAction({
-      bucket: 'portfolio',
-      path: cuid,
-      fileData: {
+  .action(async ({ parsedInput: { categoryId, media }, ctx: { session } }) => {
+    try {
+      Sentry.setUser({ id: session.user.id });
+      Sentry.setTag('action', 'uploadPortfolioItemAction');
+      Sentry.setContext('media', {
         name: media.name,
         size: media.size,
         type: media.type,
-        arrayBuffer: media.arrayBuffer,
-      },
-    });
-
-    if (!mediaUpload.data?.success) {
-      return {
-        success: false,
-        message: mediaUpload.data?.message || "Erreur lors de l'upload du fichier",
-      };
-    }
-
-    try {
-      await prisma.portfolioItem.create({
-        data: {
-          categoryId: categoryId ?? undefined,
-          id: cuid,
-          mediaId: mediaUpload.data?.data?.id,
-        },
       });
-    } catch {
+
+      if (categoryId) {
+        try {
+          const portFolioCategory = await prisma.portfolioCategory.findUnique({
+            where: { id: categoryId },
+          });
+
+          if (!portFolioCategory) {
+            return {
+              success: false,
+              message: 'Catégorie non trouvée',
+            };
+          }
+        } catch (error) {
+          Sentry.captureException(error);
+          return {
+            success: false,
+            message: 'Erreur lors de la récupération de la catégorie',
+          };
+        }
+      }
+
+      const cuid = createId();
+
+      const mediaUpload = await uploadFileAction({
+        bucket: 'portfolio',
+        path: cuid,
+        fileData: media,
+      });
+
+      if (!mediaUpload.data?.success) {
+        Sentry.captureMessage(
+          'Échec de uploadFileAction : ' + (mediaUpload.data?.message ?? 'inconnu'),
+        );
+        return {
+          success: false,
+          message: mediaUpload.data?.message || "Erreur lors de l'upload du fichier",
+        };
+      }
+
+      try {
+        await prisma.portfolioItem.create({
+          data: {
+            categoryId: categoryId ?? undefined,
+            id: cuid,
+            mediaId: mediaUpload.data.data?.id,
+          },
+        });
+      } catch (error) {
+        Sentry.captureException(error);
+        return {
+          success: false,
+          message: "Erreur lors de la création de l'élément dans le portfolio",
+        };
+      }
+
+      return {
+        success: true,
+        message: 'Élément créé avec succès',
+      };
+    } catch (error) {
+      const eventId = Sentry.captureException(error);
       return {
         success: false,
-        message: "Erreur lors de la création de l'élement dans le portfolio",
+        message: `Erreur inconnue lors de la création de l’élément dans le portfolio (${eventId})`,
       };
     }
-
-    return {
-      success: true,
-      message: 'Element créé avec succès',
-    };
   });
 
 export default uploadPortfolioItemAction;
