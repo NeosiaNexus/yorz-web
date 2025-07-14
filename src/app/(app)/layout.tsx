015529e6760a61041cc1beca2e13f6d54b34659e
NeosiaNexus <devneosianexus@gmail.com>
2025-07-14 21:30:19 +0200

    fix: Improve lighthouse SEO

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,15 +2,29 @@ import { Metadata } from 'next';
 
 import { config } from '@/lib/boiler-config';
 
-export const metadata: Metadata = {
-  title: `${config.name}`,
-  description: config.description,
-};
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: config.name,
+    description: config.description,
+    openGraph: {
+      title: config.name,
+      description: config.description,
+      images: [
+        {
+          url: `${process.env.BETTER_AUTH_URL}/images/open-graph/og-default.png`,
+          width: 422,
+          height: 255,
+          alt: 'Slogan de YorzStudio',
+        },
+      ],
+    },
+  };
+}
 
 export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>): React.JSX.Element {
-  return <main>{children}</main>;
+  return <main className="flex min-h-screen flex-col items-center justify-center">{children}</main>;
 }
