baadb7b6f748d7f1deafabd1f43f9af7b66d1295
NeosiaNexus <devneosianexus@gmail.com>
2025-07-14 21:07:38 +0200

    feat: Update server toast action to safe action

--- src/actions/toast/server-toast-action.ts
+++ src/actions/toast/server-toast-action.ts
@@ -1,18 +1,35 @@
 'use server';
 
 import { cookies } from 'next/headers';
+import { z } from 'zod';
 
-type ToastType = 'success' | 'error' | 'warning' | 'info';
+import { actionClient } from '@/lib/actions';
 
-const serverToast = async (message: string, type: ToastType = 'success'): Promise<void> => {
-  const cookieStore = await cookies();
-  const id = crypto.randomUUID();
+const inputSchema = z.object({
+  message: z.string(),
+  type: z.enum(['success', 'error', 'warning', 'info']),
+});
 
-  cookieStore.set(`toast-${id}`, JSON.stringify({ message, type }), {
-    path: '/',
-    httpOnly: true,
-    maxAge: 60 * 60 * 24,
+const outputSchema = z.object({
+  success: z.boolean(),
+});
+
+const serverToast = actionClient
+  .inputSchema(inputSchema)
+  .outputSchema(outputSchema)
+  .action(async ({ parsedInput: { message, type } }) => {
+    const cookieStore = await cookies();
+    const id = crypto.randomUUID();
+
+    cookieStore.set(`toast-${id}`, JSON.stringify({ message, type }), {
+      path: '/',
+      httpOnly: true,
+      maxAge: 60 * 60 * 24,
+    });
+
+    return {
+      success: true,
+    };
   });
-};
 
 export default serverToast;
