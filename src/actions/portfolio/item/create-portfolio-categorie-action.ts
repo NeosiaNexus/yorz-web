c14b427debe33b372f0eefec44f07985eeeea1ee
NeosiaNexus <devneosianexus@gmail.com>
2025-08-18 22:59:40 +0200

    feat(createPortfolioCategorieAction): Improve creation & rollback cloud upload

--- src/actions/portfolio/item/create-portfolio-categorie-action.ts
+++ src/actions/portfolio/item/create-portfolio-categorie-action.ts
@@ -1,40 +1,38 @@
 'use server';
 
-import { PortfolioCategory } from '@prisma/client';
+import { createId } from '@paralleldrive/cuid2';
 import { z } from 'zod';
 
-import { uploadFileAction } from '@/actions/cloud-storage-file';
+import { removeFilesAction, uploadFileAction } from '@/actions/cloud-storage-file';
 import { adminAction } from '@/lib/actions';
 import { parseZodErrors } from '@/lib/actions/parse-zod-errors';
 import prisma from '@/lib/prisma';
 
 const inputSchema = z.object({
-  title: z.string({
-    message: 'Le titre est requis',
-  }),
-  description: z.string({
-    message: 'La description est requise',
-  }),
-  underDescription: z.string().nullable(),
-  price: z.string({
+  title: z
+    .string({
+      message: 'Le titre est requis',
+    })
+    .trim()
+    .min(1, {
+      message: 'Le titre est requis',
+    }),
+  description: z.string().trim().nullable().optional(),
+  underDescription: z.string().trim().nullable().optional(),
+  price: z.string().trim().min(1, {
     message: 'Le prix est requis',
   }),
-  priceComplement: z.string().nullable(),
+  priceComplement: z.string().trim().nullable().optional(),
   colorVariant: z.enum(['blue', 'green', 'red']),
-  mediaExample: z.object({
-    name: z.string({
-      message: "Le fichier d'exemple est requis ou invalide",
-    }),
-    size: z.number().min(1, {
-      message: "Le fichier d'exemple est requis ou invalide",
-    }),
-    type: z.string({
-      message: "Le fichier d'exemple est requis ou invalide",
-    }),
-    arrayBuffer: z.instanceof(ArrayBuffer, {
-      message: "Le fichier d'exemple est requis ou invalide",
-    }),
-  }),
+  mediaExample: z
+    .object({
+      name: z.string().trim(),
+      size: z.number().min(0),
+      type: z.string(),
+      arrayBuffer: z.instanceof(ArrayBuffer),
+    })
+    .optional()
+    .nullable(),
 });
 
 const outputSchema = z.object({
@@ -57,30 +55,13 @@ const createPortfolioCategorieAction = adminAction
         underDescription,
       },
     }) => {
-      let portfolioCategory: PortfolioCategory | null = null;
-      try {
-        portfolioCategory = await prisma.portfolioCategory.create({
-          data: {
-            colorVariant,
-            price,
-            title,
-            description,
-            priceComplement,
-            underDescription,
-            order: 10,
-          },
-        });
-      } catch {
-        return {
-          success: false,
-          message: 'Erreur lors de la création de la catégorie du portfolio',
-        };
-      }
+      const categoryId = createId();
+      let mediaUpload = null;
 
-      if (mediaExample) {
-        const mediaUpload = await uploadFileAction({
+      if (mediaExample && mediaExample.size > 0) {
+        mediaUpload = await uploadFileAction({
           bucket: 'categories',
-          path: portfolioCategory.id,
+          path: categoryId,
           fileData: {
             arrayBuffer: mediaExample.arrayBuffer,
             name: mediaExample.name,
@@ -105,22 +86,42 @@ const createPortfolioCategorieAction = adminAction
             success: false,
           };
         }
+      }
 
+      try {
+        await prisma.portfolioCategory.create({
+          data: {
+            id: categoryId,
+            colorVariant,
+            price,
+            title,
+            description,
+            priceComplement,
+            underDescription,
+            mediaExampleId: mediaUpload?.data?.data?.id,
+            order: 10,
+          },
+        });
+      } catch {
         try {
-          await prisma.portfolioCategory.update({
-            where: {
-              id: portfolioCategory.id,
-            },
-            data: {
-              mediaExampleId: mediaUpload.data?.data?.id,
-            },
-          });
+          if (mediaUpload?.data?.data?.id) {
+            await removeFilesAction({
+              bucket: 'categories',
+              paths: [mediaUpload.data.data.path],
+            });
+          }
         } catch {
           return {
             success: false,
-            message: "Erreur lors de l'ajout de l'image d'exemple de la catégorie du portfolio",
+            message:
+              "Erreur lors de la suppression de l'image d'exemple de la catégorie du portfolio suite à une erreur lors de la création",
           };
         }
+
+        return {
+          success: false,
+          message: 'Erreur lors de la création de la catégorie du portfolio',
+        };
       }
 
       return {
